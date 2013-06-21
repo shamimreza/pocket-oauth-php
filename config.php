db4f1beca29f632760605994451f12610cec8285
shamimreza <shamim.bcmc@gmail.com>
2013-06-21 17:40:20 +0600

    Update config.php

--- config.php
+++ config.php
@@ -5,5 +5,5 @@
 	$redirect_uri = 'http://path.to/callback.php';
 
 	//don't edit this until you've authenticated with pocket. 
-	$access_token = 'YOUR_ACCESS_TOKEN_HERE';
+	$access_token = '15626-a1b44770bea8fa26eed6ed1a';
 ?>
